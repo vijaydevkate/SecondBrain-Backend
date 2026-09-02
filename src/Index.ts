c93636a2fc3797e7cbd8ff56eb5bd17f6758b89a
vijaydevkate <vijaydevkate98@gmail.com>
2026-09-02 12:56:54 -0400

    Created endpoints

--- src/Index.ts
+++ src/Index.ts
@@ -1,3 +1,26 @@
 import express from 'express';
 
 const app = express();
+
+app.post("/api/signup", (req, res){
+   
+})
+
+app.post("/api/signin", (req, res){
+   
+})
+
+app.post("/api/content", (req, res){
+})
+
+app.get("/api/content", (req, res){
+
+})
+
+app.delete("/api/content", (req, res){
+
+})
+
+app.get("/api/share", (req, res){
+
+})
